26074f1c62568e10daf6a9a4e80cc19e78c9ed3a
hitesha1981 <hitesha1981@gmail.com>
2016-02-20 11:57:23 +0800

    Adding Audit Scripts

---
+++ checks.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python
+'''
+A script which was written to perform audit of hosts to make sure they have standard setup in place to conform to standard monitoring and configuration management requirements of the organization.
+Scripts arguments are input of IPs in a file. Output of the results of audit are redirected to a csv file.
+
+Script uses Boolean values to capture the success/failure of a check. it also employs SSH key to be added automatically as the script runs through a chunk of hosts
+
+Example output: argument file has 1 ip. First part is the verbose output as script executes and the second is fetched from the file where the results were written.
+[root@srv0001 python]# ./checks.py ips.txt
+Host is up & running
+Warning: Permanently added '172.16.46.90' (RSA) to the list of known hosts.
+Puppet agent is running
+Sysedge agent is running
+Trap destinations are fine
+172.16.46.90 is in Smarts
+['qa1654', True, True, True, True, True, ['STRING: "crond"', 'STRING: "ntpd"', 'STRING: "sshd"', 'STRING: "syslogd"', 'STRING: "cron"'], (True, '31')]
+[root@srv0001 python]# tail -1 stats.csv
+qa1654,True,True,True,True,True,"['STRING: ""crond""', 'STRING: ""ntpd""', 'STRING: ""sshd""', 'STRING: ""syslogd""', 'STRING: ""cron""']","(True, '31')"
+
+'''
+import subprocess
+import shlex
+import os
+import csv
+import sys
+
+dc = '' 
+trapd = []
+
+# Trap receivers of SNMP traps are DC specific & according to the DC identified, the hosts will be validated against these values.
+def dcid(hostname):
+    global dc
+    global trapd
+    if re.search(r'.*\.(mon2|mgmt2)\..*', hostname):
+	dc = '2'
+	trapd = ['172.16.26.230','fm-dn01a.c0.mon2.adsk','172.16.26.231','fm-dn01b.c0.mon2.adsk','172.25.185.242','fm-sn01a.c0.mon2.adsk','172.25.185.243','fm-sn01b.c0.mon2.adsk']
+    elif re.search(r'.*\.(mon4|mgmt4)\..*', hostname):
+	dc = '4'
+	trapd = ['172.16.29.230','bm-dn01a.mon4.adsk','172.16.29.231','bm-dn01b.mon4.adsk','172.26.185.242','bm-sn01a.mon4.adsk','172.26.185.243','bm-sn01b.mon4.adsk']
+    elif re.search(r'.*\.(mon5|mgmt5)\..*', hostname):
+	dc = '5'
+	trapd = ['172.16.32.210','montraps5001a.mgmt5.adsk','172.16.32.211','montraps5001b.mgmt5.adsk']
+    elif re.search(r'.*\.(mon6|mgmt6)\..*', hostname):
+        dc = '6'
+	trapd = ['172.16.35.210','montraps6001a.mgmt6.adsk','172.16.35.211','montraps6001b.mgmt6.adsk']
+    elif re.search(r'.*\.(mon7|mgmt7)\..*', hostname):
+        dc = '7'
+	trapd = ['172.16.38.230','dn7001a.mgmt7.adsk','172.16.38.231','dn7001b.mgmt7.adsk','172.18.185.242','sn7001a.mon7.adsk','172.18.185.243','sn7001b.mon7.adsk']
+    elif re.search(r'.*\.mon10\..*', hostname):
+        dc = '10'
+	trapd = ['172.16.49.22','montraps10001a.mon10.adsk','172.16.49.23','montraps10001b.mon10.adsk']
+    elif re.search(r'.*\.mon11\..*', hostname):
+        dc = '11'
+	trapd = ['172.16.46.28','montraps11001a.mon11.adsk','172.16.46.29','montraps11001b.mon11.adsk']
+    elif re.search(r'.*\.mon20\..*', hostname):
+        dc = '20'
+	trapd = ['172.16.43.149','montraps20001a.mon20.adsk','172.16.43.150','montraps20001b.mon20.adsk']
+    elif re.search(r'.*\.mon21\..*', hostname):
+        dc = '21'
+	trapd = ['172.16.44.154','montraps21001a.mon21.adsk','172.16.44.155','montraps21001b.mon21.adsk']
+    else:
+    	print("DC not known")
+	exit
+
+
+#pingable = puppet_agent = sysedge_agent = sysedge_config = in_smarts = audit_config = in_ehealth = False
+
+# as the name suggests this function checks if a host is pingable or not, use of Try/Except clauses to determine the same.
+def pingcheck(ip):
+    global pingable,puppet_agent,sysedge_agent,sysedge_config,in_smarts,audit_config,in_ehealth
+    pingable = puppet_agent = sysedge_agent = sysedge_config = in_smarts = audit_config = in_ehealth = False
+    cmd1 = 'ping -c 3 ' + ip
+
+    try:
+        cmd1op = subprocess.check_output(shlex.split(cmd1)).splitlines()[-2].find('0% packet loss')
+        if cmd1op != -1:
+              pingable = True
+              print("Host is up & running")
+        else:
+              print("Host is not running")
+        return pingable
+    except subprocess.CalledProcessError:
+      print("{} is not reachable").format(ip)
+      return pingable
+
+#Puppet agent running on the hosts is an important part of the audit, this function does that. 
+def puppetagent(ip):
+    global puppet_agent;puppet_agent = False
+    cmd2 = 'ssh -i /home/hitesha/id_dsa -o StrictHostKeyChecking=no ' + ip + ' "service puppet status"'
+    try:
+	cmd2op = subprocess.check_output(shlex.split(cmd2)).find('is running...')
+    except subprocess.CalledProcessError:
+	return puppet_agent
+    if cmd2op != -1:
+        puppet_agent = True
+        print("Puppet agent is running")
+    else:
+        print("Puppet agent is not running")
+    return puppet_agent
+
+#Function to check if SNMP agent from vendor CA i.e sysedge is running on the host or not
+def sysedgeagent(ip):
+    global sysedge_agent;sysedge_agent = False
+    cmd3 = 'ssh -i /home/hitesha/id_dsa -o StrictHostKeyChecking=no ' + ip + ' "service sysedge status"'
+    cmd3op = subprocess.check_output(shlex.split(cmd3)).find('SystemEDGE is running')
+    if cmd3op != -1:
+        sysedge_agent = True
+        print("Sysedge agent is running")
+    else:
+        print("Sysedge agent is not running")
+    return sysedge_agent
+
+#Function to check if the SNMP agent is configured properly to redirect traps to the correct trap receiver hosts in that specific DC.
+def sysedgeconfig(ip):
+    global sysedge_config, trapd, dc
+    sysedge_config = False
+    cmd4 = 'ssh -i /home/hitesha/id_dsa -o StrictHostKeyChecking=no ' + ip + ' "grep trap_community /etc/sysedge.cf | grep public"'
+    cmd4op = subprocess.check_output(shlex.split(cmd4))
+    if dc in ('2','4','7'):
+        if ip[0:3] == '192':
+            if cmd4op.splitlines()[0].split(" ")[2] in (trapd[0],trapd[1],trapd[2],trapd[3]) and cmd4op.splitlines()[1].split(" ")[2] in (trapd[0],trapd[1],trapd[2],trapd[3]):
+                sysedge_config = True
+                print("Trap destinations are fine")
+            else:
+                print("Trap destination are misconfigured")
+        else:
+            if cmd4op.splitlines()[0].split(" ")[2] in (trapd[4],trapd[5],trapd[6],trapd[7]) and cmd4op.splitlines()[1].split(" ")[2] in (trapd[4],trapd[5],trapd[6],trapd[7]):
+                sysedge_config = True
+                print("Trap destinations are fine")
+            else:
+                print("Trap destination are misconfigured")
+    else:
+        if cmd4op.splitlines()[0].split(" ")[2] in (trapd[0],trapd[1],trapd[2],trapd[3]) and cmd4op.splitlines()[1].split(" ")[2] in (trapd[0],trapd[1],trapd[2],trapd[3]):
+            sysedge_config = True
+            print("Trap destinations are fine")
+        else:
+            print("Trap destination are misconfigured")
+    return sysedge_config
+
+# Function to check if the system is being monitored in EMC Smarts or not, it utilises a call to another short script that checks this.
+def smartscheck(ip):
+    global in_smarts;in_smarts = False
+    cmd5 = 'ssh -i /home/hitesha/id_dsa gn11001a.mon11.adsk "/opt/InCharge/SAM/smarts/bin/sm_perl /opt/smarts-scripts/findIP/findip.pl --ip=' + ip + ' --config=/opt/smarts-scripts/findIP/config.txt | grep exists " '
+    try:
+        cmd5op = subprocess.check_output(shlex.split(cmd5)).find('true')
+    except subprocess.CalledProcessError:
+        cmd5op = subprocess.check_output(shlex.split(cmd5)).find('true')
+    if cmd5op != -1:
+     in_smarts = True
+     print("{} is in Smarts").format(ip)
+    else:
+     print("{} Not in Smarts").format(ip)
+    return in_smarts
+
+# Function to check if the hosts is under Auditron(an in house monitoring tool that takes care of process monitoring.
+def auditron(ip):
+    global audit_config;audit_config = False
+    cmd6 = 'ssh -i /home/hitesha/id_dsa jump11001a.mon11.adsk "/home/tmyra/ops_tools/procmon/procmon-showall2.sh ' + ip + '"'
+    cmd6op = subprocess.check_output(shlex.split(cmd6)).splitlines()
+    return cmd6op
+
+# Function to check if the host IP is being monitored in CA eHealth systems, basically meant for Hardware and Filesystems, partition monitoring. this is done by checking the mysql DB for existence of elements & getting a count of those elements for the host.
+def ehealth(ip):
+    global in_ehealth;in_ehealth = False
+    cmd7 = 'mysql -h miscdb5001a.mgmt5.adsk -u \'EhAutomation\' -p\'EhAutomation\' EhealthAutomation --skip-column-names -e "select count(name) from ElementsDatabase where ipAddr like \'' + ip + ':%\';"'
+    cmd7op = subprocess.check_output(shlex.split(cmd7)).strip()
+    if cmd7op == '0':
+        print('{} is not in Ehealth').format(ip)
+    else:
+        in_ehealth = True
+    return in_ehealth,cmd7op
+
+#Run throught the list of IPs from the input file one by one and output the output to a csv file. If the hosts itself is down, there is no need to run through the other checks.
+iplist = sys.argv[1]
+with open(iplist,'r') as file:
+        for ip in file.readlines():
+	    ip_stat = []
+	    hostcmd = 'dig -x ' + ip + ' +short'
+	    hostname = subprocess.check_output(shlex.split(hostcmd)).strip()
+	    ip_stat.append(hostname)
+	    dcid(hostname)
+            pingable = pingcheck(ip.strip())
+            if pingable == False:
+                ip_stat.append(pingable)
+                ip_stat.append(puppet_agent)
+                ip_stat.append(sysedge_agent)
+                ip_stat.append(sysedge_config)
+                ip_stat.append(in_smarts)
+                ip_stat.append(audit_config)
+		ip_stat.append(in_ehealth)
+		print ip_stat
+                with open('stats.csv','ab') as myfile:
+                  myfilewriter = csv.writer(myfile)
+                  myfilewriter.writerow(ip_stat)
+            else:
+                ip_stat.append(pingable)
+                ip_stat.append(puppetagent(ip.strip()))
+                ip_stat.append(sysedgeagent(ip.strip()))
+                ip_stat.append(sysedgeconfig(ip.strip()))
+                ip_stat.append(smartscheck(ip.strip()))
+                ip_stat.append(auditron(ip.strip()))
+		ip_stat.append(ehealth(ip.strip()))
+                print ip_stat
+                with open('stats.csv','ab') as myfile:
+                  myfilewriter = csv.writer(myfile)
+                  myfilewriter.writerow(ip_stat)
