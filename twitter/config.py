c19d0b4f0dfa5e2bfb3c23f144dcf91e97ef8b6c
hitesha1981 <hitesha1981@gmail.com>
2016-03-02 11:54:40 +0800

    Adding code for twitter tweet list and updating the status

---
+++ twitter/config.py
@@ -0,0 +1,9 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+CONSUMER_KEY = 'XXXXXXXXXXXXXXXXXXXXXXX'
+CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXX'
+ACCESS_KEY = 'XXXXXXXXXXXXXXXXXXXXXXX'
+ACCESS_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXX'
